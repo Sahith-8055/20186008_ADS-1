2f08383f74878348161b593de89ba4ff8b99fbea
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-10-04 20:51:37 +0530

    "ads1_m10_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ASSIGNMENTS/M10/10.1 LinkedList using Recursion/Solution.java
+++ ASSIGNMENTS/M10/10.1 LinkedList using Recursion/Solution.java
@@ -1,21 +1,63 @@
 import java.util.Scanner;
+/**
+ * Class of linked list.
+ */
 class LinkedList {
+    /**
+     * {Variable head of node type}.
+     */
     private Node head;
+    /**
+     * {Size of linked list}.
+     */
     private int size;
+    /**
+     * Class for node.
+     */
     private class Node {
+        /**
+         * {Integer item}.
+         */
         private int item;
+        /**
+         * {Variable next of node type}.
+         */
         private Node next;
+        /**
+         * Constructs the object.
+         * Time complexity of this method is O(1).
+         * @param      i     {Integer}
+         * @param      n     {Node}
+         */
         Node(final int i, final Node n) {
             this.item = i;
             this.next = n;
         }
     }
+    /**
+     * Constructs the object.
+     * Time complexity of this method is O(1).
+     */
     LinkedList() {
         this.size = 0;
     }
+    /**
+     * Method to get the size.
+     * Time complexity of this method is O(1).
+     * @return     The size.
+     */
     public int getSize() {
         return this.size;
     }
+    /**
+     * {Method to insert an element}.
+     * Time complexity of this method is O(N).
+     * @param      head     The head
+     * @param      count    The count
+     * @param      element  The element
+     *
+     * @return     {Node}
+     */
     public Node insert(final Node head, final int count, final int element) {
         if (count == 0) {
             return new Node(element, head);
@@ -24,9 +66,22 @@ class LinkedList {
         size++;
         return head;
     }
+    /**
+     * {Method to insert at a particular position}.
+     * Time complexity of this method is O(1).
+     * @param      pos    The position
+     * @param      item1  The item 1
+     */
     public void insertAt(final int pos, final int item1) {
         head = insert(head, pos, item1);
     }
+    /**
+     * {Method for reverse}.
+     * Time complexity of this method is O(N).
+     * @param      head  The head
+     *
+     * @return     {Node}
+     */
     public Node Reverse(Node head) {
         if (head == null || head.next == null) {
             return head;
@@ -36,9 +91,18 @@ class LinkedList {
         head.next = null;
         return nextHead;
     }
+    /**
+     * {Method to reverse a given linked list}.
+     * Time complexity of this method is O(1).
+     */
     public void reverse() {
         head = Reverse(head);
     }
+    /**
+     * {Method to display the elements}.
+     * Time complexity of this method is O(N).
+     * @return     {String}
+     */
     public String display() {
         Node old = head;
         String str = "";
@@ -49,10 +113,21 @@ class LinkedList {
         return str.substring(0, str.length() - 2);
     }
 }
+/**
+ * Class for solution.
+ */
 public class Solution {
+    /**
+     * Constructs the object.
+     */
     private Solution() {
         //Unused Constructor.
     }
+    /**
+     * {Client Program}.
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         LinkedList ll = new LinkedList();
