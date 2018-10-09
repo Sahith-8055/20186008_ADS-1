b3f65a159238c4b1a02b0abd7ea85c7593607498
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-10-09 12:00:40 +0530

    "ads1_m14_a2: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ASSIGNMENTS/M14/14.2 student reservation system/Solution.java
+++ ASSIGNMENTS/M14/14.2 student reservation system/Solution.java
@@ -1,15 +1,53 @@
 import java.util.Scanner;
 import java.util.Date;
 import java.text.SimpleDateFormat;
+/**
+ * Class for student.
+ */
 class Student implements Comparable<Student> {
+    /**
+     * {Name of the student}.
+     */
     private String studentName;
+    /**
+     * {Date of birth}.
+     */
     private Date dob;
+    /**
+     * {Marks in subject1}.
+     */
     private int marks1;
+    /**
+     * {Marks in subject2}.
+     */
     private int marks2;
+    /**
+     * {Marks in subject3}.
+     */
     private int marks3;
+    /**
+     * {Total marks}.
+     */
     private int totalmarks;
+    /**
+     * {Category}.
+     */
     private String category;
+    /**
+     * {Student gets admission (or) not}.
+     */
     private boolean allocation;
+    /**
+     * Constructs the object.
+     *
+     * @param      name  The name
+     * @param      dob1  The dob1
+     * @param      m1    The m1
+     * @param      m2    The m2
+     * @param      m3    The m3
+     * @param      t     {Total marks}
+     * @param      cat   The category
+     */
     Student(final String name, final String dob1, final int m1,
             final int m2, final int m3, final int t, final String cat) {
         this.studentName = name;
@@ -26,54 +64,141 @@ class Student implements Comparable<Student> {
         this.category = cat;
         this.allocation = false;
     }
+    /**
+     * Gets the allocation.
+     * Time complexity of this method is O(1).
+     * @return     The allocation.
+     */
     public boolean getAllocation() {
         return this.allocation;
     }
+    /**
+     * Sets the allocation.
+     * Time complexity of this method is O(1).
+     * @param      allot  The allot
+     */
     public void setAllocation(final boolean allot) {
         this.allocation = allot;
     }
+    /**
+     * Gets the student name.
+     * Time complexity of this method is O(1).
+     * @return     The student name.
+     */
     public String getStudentName() {
         return this.studentName;
     }
+    /**
+     * Sets the student name.
+     * Time complexity of this method is O(1).
+     * @param      student1  The student1
+     */
     public void setStudentName(final String student1) {
         this.studentName = student1;
     }
+    /**
+     * Gets the m1.
+     * Time complexity of this method is O(1).
+     * @return     The m1.
+     */
     public int getM1() {
         return this.marks1;
     }
+    /**
+     * Sets the m1.
+     * Time complexity of this method is O(1).
+     * @param      mark1  The mark 1
+     */
     public void setM1(final int mark1) {
         this.marks1 = mark1;
     }
+    /**
+     * Gets the m2.
+     * Time complexity of this method is O(1).
+     * @return     The m 2.
+     */
     public int getM2() {
         return this.marks2;
     }
+    /**
+     * Sets the m2.
+     * Time complexity of this method is O(1).
+     * @param      mark2  The mark 2
+     */
     public void setM2(final int mark2) {
         this.marks2 = mark2;
     }
+    /**
+     * Gets the m3.
+     * Time complexity of this method is O(1).
+     * @return     The m 3.
+     */
     public int getM3() {
         return this.marks3;
     }
+    /**
+     * Sets the m3.
+     * Time complexity of this method is O(1).
+     * @param      mark3  The mark 3
+     */
     public void setM3(final int mark3) {
         this.marks3 = mark3;
     }
+    /**
+     * Gets the dob.
+     * Time complexity of this method is O(1).
+     * @return     The dob.
+     */
     public Date getDOB() {
         return this.dob;
     }
+    /**
+     * Sets the dob.
+     * Time complexity of this method is O(1).
+     * @param      d     {Date object}
+     */
     public void setDOB(final Date d) {
         this.dob = d;
     }
+    /**
+     * Gets the reservation.
+     * Time complexity of this method is O(1).
+     * @return     The reservation.
+     */
     public String getReservation() {
         return this.category;
     }
+    /**
+     * Sets the category.
+     * Time complexity of this method is O(1).
+     * @param      c     {String}
+     */
     public void setCategory(final String c) {
         this.category = c;
     }
+    /**
+     * Gets the total.
+     * Time complexity of this method is O(1).
+     * @return     The total.
+     */
     public int getTotal() {
         return this.totalmarks;
     }
+    /**
+     * Sets the total.
+     * Time complexity of this method is O(1).
+     * @param      tm    The time
+     */
     public void setTotal(final int tm) {
         this.totalmarks = tm;
     }
+    /**
+     * {Method to compare two objects}
+     * Time complexity of this method is O(1).
+     * @param      that  The that
+     *
+     * @return     {Integer}
+     */
     public int compareTo(final Student that) {
         if (this.getTotal() > that.getTotal()) {
             return -1;
@@ -98,6 +223,11 @@ class Student implements Comparable<Student> {
         }
         return 0;
     }
+    /**
+     * Returns a string representation of the object.
+     * Time complexity of this method is O(N).
+     * @return     String representation of the object.
+     */
     public String toString() {
         StringBuffer sb = new StringBuffer();
         sb.append(this.getStudentName() + ",");
@@ -106,15 +236,38 @@ class Student implements Comparable<Student> {
         return sb.toString();
     }
 }
+/**
+ * Class for insertion sort.
+ */
 class InsertionSort {
+    /**
+     * {Method to find which among the two is less}.
+     *
+     * @param      v     {Comparable object}
+     * @param      w     {Comparable object}
+     * Time complexity of this method is O(1).
+     * @return     {Boolean value}
+     */
     private static boolean less(final Comparable v, final Comparable w) {
         return v.compareTo(w) < 0;
     }
+    /**
+     * {Method to exchange two objects}.
+     * Time complexity of this method is O(N).
+     * @param      a     {Comparable array}
+     * @param      i     {Integer i}
+     * @param      j     {Integer j}
+     */
     private static void exch(final Comparable[] a, final int i, final int j) {
         Comparable temp = a[i];
         a[i] = a[j];
         a[j] = temp;
     }
+    /**
+     * {Method for insertion sort}.
+     * Time complexity of this method is O(N^2).
+     * @param      a     {Comparable array}
+     */
     public static void sort(final Comparable[] a) {
         for (int i = 0; i < a.length; i++) {
             for (int j = i; j > 0 && less(a[j], a[j - 1]); j--) {
@@ -123,10 +276,21 @@ class InsertionSort {
         }
     }
 }
+/**
+ * Class for solution.
+ */
 public class Solution {
+    /**
+     * Constructs the object.
+     */
     private Solution() {
         //Unused Constructor.
     }
+    /**
+     * {Client Program}.
+     *
+     * @param      args  The arguments
+     */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = Integer.parseInt(scan.nextLine());
@@ -150,12 +314,27 @@ public class Solution {
         print(students);
         counselling(students, vacancies, unreservedSeats, bcSeats, scSeats, stSeats);
     }
+    /**
+     * {Method to print}.
+     * Time complexity of this method is O(N).
+     * @param      s     {Student array}
+     */
     public static void print(final Student[] s) {
         for (Student stu : s) {
             System.out.println(stu);
         }
         System.out.println();
     }
+    /**
+     * {Method for counselling}.
+     * Time complexity of this method is O(N^2).
+     * @param      students         The students
+     * @param      vacancies        The vacancies
+     * @param      unreservedSeats  The unreserved seats
+     * @param      bcSeats          The bc seats
+     * @param      scSeats          The screen seats
+     * @param      stSeats          The st seats
+     */
     public static void counselling(final Student[] students, int vacancies,
                                    int unreservedSeats, int bcSeats, int scSeats, int stSeats) {
         int i = 0;
